30285614ff20df58a22715a2d9f5c321822c8e42
Home Ethan Schoemaker <e.schoemaker@outlook.com>
2016-09-28 14:17:37 +0930

    no message

---
+++ Assets/LightbeamScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightbeamScript : MonoBehaviour {
+    public Material beamMaterial;
+    public Shader shader;
+    public Texture texture;
+    public Color color;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        shader = beamMaterial.shader;
+        color = Color.red;
+
+        Renderer rend = GetComponent<Renderer>();
+        rend.material = new Material(shader);
+        rend.material.mainTexture = beamMaterial.mainTexture;
+        rend.material.SetColor("_TintColor", color);
+        
+        //beamMaterial = GetComponent<Material>();
+        //this.beamMaterial.SetColor("_TintColor", Color.blue);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+}
