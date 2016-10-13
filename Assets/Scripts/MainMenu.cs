2ba99020a44d7a4a5807720287f660ba37aaee80
Tyler Roach <tylerroach3@gmail.com>
2016-10-14 10:08:23 +1030

    Refined map layout/design. Re-implemented puzzles.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public GameObject loadScreen;
+
+	// Use this for initialization
+	void Start () {
+		loadScreen.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void NewGame() {
+		loadScreen.SetActive (true);
+		Application.LoadLevel (0);
+	}
+
+	public void Exit () {
+		Application.Quit ();
+	}
+}
