1e18032bb17f49dfeb50ca360710b00f9f93b965
Ethan Schoemaker <ethan.schoemake@ad.aie.edu>
2016-10-19 16:09:56 +1030

    new save game assets

--- Assets/Scripts/SaveGame.cs
+++ Assets/Scripts/SaveGame.cs
@@ -2,15 +2,18 @@
 using System.Collections;
 using InControl;
 
+
 public class SaveGame : MonoBehaviour
 {
     public GameObject player;
     MyCharacterActions characterActions;
+    Health playerHealth;
 
     // Use this for initialization
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<Health>();
         characterActions = new MyCharacterActions();
         characterActions.save.AddDefaultBinding(Key.F1);
         characterActions.load.AddDefaultBinding(Key.F2);
@@ -19,23 +22,37 @@ public class SaveGame : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (characterActions.save.IsPressed)
         {
+            ES2.Save(transform, "myFile");
+            ES2.Save(player.transform.position, "myFile.txt?tag=playerPosition");
+            ES2.Save(playerHealth.currenthealth, "myFile.txt?tag=playerHealth");
+            /*
             PlayerPrefs.SetFloat("PlayerX", player.transform.position.x);
             PlayerPrefs.SetFloat("PlayerY", player.transform.position.y);
             PlayerPrefs.SetFloat("PlayerZ", player.transform.position.z);
             PlayerPrefs.Save();
+            */
         }
         if(characterActions.load.IsPressed)
         {
+            if (ES2.Exists("myFile"))
+            {
+                player.transform.position = ES2.Load<Vector3>("myFile.txt?tag=playerPosition");
+                playerHealth.currenthealth = ES2.Load<int>("myFile.txt?tag=playerHealth");
+            }
+            /*
             Vector3 tempPos;
             tempPos.x = PlayerPrefs.GetFloat("PlayerX");
             tempPos.y = PlayerPrefs.GetFloat("PlayerY");
             tempPos.z = PlayerPrefs.GetFloat("PlayerZ");
             player.transform.position = tempPos;
+            */
         }
 
     }
+    
 }
 
 
@@ -45,8 +62,6 @@ public class SaveGame : MonoBehaviour
  * THINGS TO SAVE * 
  * player position
  * player health
- * player mana
- * player stamina 
  * mirror rotations
  * light source rotations
  * player level?
