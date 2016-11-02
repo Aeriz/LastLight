323d7440901a9d0c1bbb2a1c3588781c82cc3430
Tyler Roach <tylerroach3@gmail.com>
2016-11-02 16:34:16 +1030

    Added gate and edited puzzles

---
+++ Assets/Scripts/PyxisReticulumManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using VolumetricLines;
+
+public class PyxisReticulumManager : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Puzzle").GetComponent<Puzzle>().puzzleComplete == true)
+        {
+            anim.SetBool("GateRight", true);
+        }
+    }
+}
