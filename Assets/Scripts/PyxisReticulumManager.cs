77be523c7eaf162a0289d509021d4850797fa746
Tyler Roach <tylerroach3@gmail.com>
2016-11-10 16:04:01 +1030

    Worked more to tutorial

--- Assets/Scripts/PyxisReticulumManager.cs
+++ Assets/Scripts/PyxisReticulumManager.cs
@@ -12,7 +12,8 @@ public class PyxisReticulumManager : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        StartCoroutine("Companion", 10.0f);
+        companionText.text = "";
+        StartCoroutine("Companion", 5.0f);
     }
 
     // Update is called once per frame
@@ -27,8 +28,37 @@ public class PyxisReticulumManager : MonoBehaviour
 
     IEnumerator Companion(float waitTime)
     {
-        companionText.text = "test test test";
+        if (PlayerPrefs.GetInt("TutorialDone") == 0)
+        {
+            companionText.text = "";
+            yield return new WaitForSeconds(1.5f);
+            companionText.text = "It is finally you... \n The one we have been waiting for.";
+            yield return new WaitForSeconds(waitTime);
+            companionText.text = "We haven't much time. \n Take this Prism and head down the ramp.";
+            yield return new WaitForSeconds(waitTime);
+            companionText.text = "";
+        }
+    }
+
+    IEnumerator CompanionTwo(float waitTime)
+    {
+        companionText.text = "";
         yield return new WaitForSeconds(waitTime);
-        companionText.text = "test2 test2 test2";
+        companionText.text = "Only you can use this Prism. \n With it you can manipulate elements of light.";
+        yield return new WaitForSeconds(waitTime);
+        companionText.text = "Go forth. Test its power. Understand it.";
+        yield return new WaitForSeconds(waitTime);
+        companionText.text = "Unlock the long-locked gates to Perseus!";
+        yield return new WaitForSeconds(waitTime);
+        companionText.text = "";
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            Debug.Log("PLAYER ENTERED");
+            StartCoroutine("CompanionTwo", 5.0f);
+        }
     }
 }
