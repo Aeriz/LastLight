03594d2cc3abbb9b7062c7cc42dea90ede3b306f
Tyler Roach <tylerroach3@gmail.com>
2016-11-29 14:52:21 +1030

    New textures and kill boxes in

---
+++ Assets/Scripts/KillBox.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillBox : MonoBehaviour {
+
+    public GameObject loadScreen;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            loadScreen.SetActive(true);
+            Application.LoadLevel(Application.loadedLevel);
+        }
+    }
+}
